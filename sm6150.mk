0b54fd88babfeca02ef908c85913a2b4ce295e1c
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2025-01-16 22:33:05 +0200

    Remove OMX Audio Codecs

--- sm6150.mk
+++ sm6150.mk
@@ -194,11 +194,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
